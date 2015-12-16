280fcae9ca5350a816c850ac6ffe6224e33f843a
oded-soffrin <oded.soffrin@gmail.com>
2015-12-16 17:55:28 +0200

    oauth2

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -9,27 +9,7 @@ chrome.browserAction.setBadgeText({ text: '\'Allo' });
 
 var api = 'http://localhost:1337'; //'http://misha-api.herokuapp.com'
 
-
-
-//OAUTH stuff
-chrome.identity.getAuthToken({ 'interactive': true }, function (token) {
-  //load Google's javascript client libraries
-
-  var x = new XMLHttpRequest();
-  x.open('GET', 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token);
-  x.onload = function () {
-    alert(x.response);
-    var parsed_response = JSON.parse(x.response);
-    $.get(api + '/user?email=' + parsed_response.email, function (res) {
-      chrome.storage.local.set({me: res[0]});
-      console.log('got', res);
-    });
-  };
-  x.send();
-});
-
 setInterval(function () {
-
   $.post(api + '/user/seen?user_id=567151fe7d2baa1d49c0dcfa', function (res) {
     console.log("got response", res);
     console.count("got response");
@@ -49,4 +29,18 @@ setInterval(function () {
   });
 }, 12000);
 
+//OAUTH stuff
+chrome.identity.getAuthToken({ 'interactive': true }, function (token) {
+  //load Google's javascript client libraries
+
+  var x = new XMLHttpRequest();
+  x.open('GET', 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' + token);
+  x.onload = function () {
+    var parsed_response = JSON.parse(x.response);
+    $.get(api + '/user?email=' + parsed_response.email, function (res) {
+      chrome.storage.local.set({ me: res[0] });
+    });
+  };
+  x.send();
+});
 //# sourceMappingURL=background.js.map
