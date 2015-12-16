f5e174ec2838dc5c9ae154949c3dd5495df52945
Guy Sopher <GuySopher@gmail.com>
2015-12-16 17:15:23 +0200

    notification when back

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -10,5 +10,21 @@ chrome.browserAction.setBadgeText({text: '\'Allo'});
 var api = 'http://localhost:1337'; //'http://misha-api.herokuapp.com'
 
 setInterval(function(){
-  $.post(api + '/user/seen?email=guyso@wix.com');
-}, 15000);
+  $.post(api + '/user/seen?user_id=567151fe7d2baa1d49c0dcfa', function(res) {
+    console.log("got response", res);
+    console.count("got response");
+
+    if (res.notify) {
+      chrome.notifications.create('temp', {
+        type: "basic",
+        iconUrl: "images/icon-128.png",
+        title: res.message,
+        message: "Default Message",
+        expandedMessage: " Expanded message"
+
+      }, function(data) {
+        console.log('Notification callback', data);
+      })
+    }
+  });
+}, 12000);
